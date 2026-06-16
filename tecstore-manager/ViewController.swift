2f89f5cf6dc904c688d721a42d38812b99560ff1
romero060523 <andy.romero@tecsup.edu.pe>
2026-06-16 15:46:41 -0500

    Initial Commit

---
+++ tecstore-manager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tecstore-manager
+//
+//  Created by Jhonatan on 16/06/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
